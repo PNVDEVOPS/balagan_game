c97eb9e4e3497c24df1bb1874f96090ddc41661b
Loonquat <https://repos.21-school.ru>
2026-05-18 23:38:22 +0900

    feat: audio manager with ambient fading, multi-channel SFX, and breathing intensity

--- scripts/autoload/audio_manager.gd
+++ scripts/autoload/audio_manager.gd
@@ -1,24 +1,58 @@
 extends Node
 
 var ambient_player: AudioStreamPlayer
-var sfx_player: AudioStreamPlayer
+var sfx_players: Array[AudioStreamPlayer] = []
+var breathing_player: AudioStreamPlayer
+
+const MAX_SFX_PLAYERS := 4
+const RANDOM_SCARE_CHANCE := 0.1
 
 func _ready() -> void:
 	ambient_player = AudioStreamPlayer.new()
 	ambient_player.bus = "Master"
+	ambient_player.volume_db = -6.0
 	add_child(ambient_player)
 
-	sfx_player = AudioStreamPlayer.new()
-	sfx_player.bus = "Master"
-	add_child(sfx_player)
+	breathing_player = AudioStreamPlayer.new()
+	breathing_player.bus = "Master"
+	breathing_player.volume_db = -20.0
+	add_child(breathing_player)
 
-func play_sfx(stream: AudioStream) -> void:
-	sfx_player.stream = stream
-	sfx_player.play()
+	for i in MAX_SFX_PLAYERS:
+		var p := AudioStreamPlayer.new()
+		p.bus = "Master"
+		add_child(p)
+		sfx_players.append(p)
 
-func play_ambient(stream: AudioStream) -> void:
+func play_sfx(stream: AudioStream, volume_db: float = 0.0) -> void:
+	for p in sfx_players:
+		if not p.playing:
+			p.stream = stream
+			p.volume_db = volume_db
+			p.play()
+			return
+
+func play_ambient(stream: AudioStream, fade_in: float = 1.0) -> void:
+	if ambient_player.playing:
+		var tween := create_tween()
+		tween.tween_property(ambient_player, "volume_db", -40.0, fade_in * 0.5)
+		await tween.finished
 	ambient_player.stream = stream
+	ambient_player.volume_db = -40.0
 	ambient_player.play()
+	var tween := create_tween()
+	tween.tween_property(ambient_player, "volume_db", -6.0, fade_in)
 
-func stop_ambient() -> void:
+func stop_ambient(fade_out: float = 1.0) -> void:
+	var tween := create_tween()
+	tween.tween_property(ambient_player, "volume_db", -40.0, fade_out)
+	await tween.finished
 	ambient_player.stop()
+
+func set_breathing_intensity(intensity: float) -> void:
+	breathing_player.volume_db = lerpf(-40.0, -8.0, clampf(intensity, 0.0, 1.0))
+
+func maybe_play_random_scare(sounds: Array[AudioStream]) -> void:
+	if randf() < RANDOM_SCARE_CHANCE and not sounds.is_empty():
+		var sound: AudioStream = sounds[randi() % sounds.size()]
+		play_sfx(sound, -12.0)
